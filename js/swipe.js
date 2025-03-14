8c08d399913568c25f41ebb70b470d2f3dcf3e7f
ENOCH <enoch.apaza@gmail.com>
2025-03-14 15:07:22 -0300

    Update swipe.js

--- js/swipe.js
+++ js/swipe.js
@@ -1,1 +1,19 @@
+let touchStartX = 0;
+let touchEndX = 0;
+
+document.addEventListener("touchstart", function(event) {
+  touchStartX = event.changedTouches[0].screenX;
+}, false);
+
+document.addEventListener("touchend", function(event) {
+  touchEndX = event.changedTouches[0].screenX;
+  handleSwipe();
+}, false);
+
+function handleSwipe() {
+  if (touchStartX - touchEndX > 50) {
+    // Deslizou para a esquerda
+    window.location.href = "https://SEU_USUARIO.github.io/NOME-DO-REPOSITORIO/index.html";
+  }
+}
 
